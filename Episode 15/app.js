65e36e16a06af0647b12177a7f80f960f97924eb
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-04-18 18:15:17 +0200

    Episode 15

--- Episode 15/app.js
+++ Episode 15/app.js
@@ -1,8 +1,14 @@
 Vue.component('greeting', {
-    template: '<p>Hey there, I am a re-usable component</p>',
+    template: '<p>Hey there, I am {{ name }} . <button v-on:click="changeName">Change name</button></p>',
     data: function() {
         return {
-            
+            name: 'Yoshi'
+        }
+    },
+
+    methods: {
+        changeName: function() {
+            this.name = 'Mario';
         }
     }
 });

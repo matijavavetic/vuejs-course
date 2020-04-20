1f89d5161e44df18eb94ea0fe0d437aaaa216e1b
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-04-20 16:39:10 +0200

    Episode 28 - Dynamic components

--- Episode 27/vuejs/src/App.vue
+++ Episode 27/vuejs/src/App.vue
@@ -1,37 +1,38 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is the title of the form</h3>
-        <p>Information about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="Name" required/>
-        <input type="password" placeholder="Password" required/>
-      </div>
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show form one</button>
+    <button v-on:click="component = 'form-two'">Show form two</button>
   </div>
 </template>
 
 <script>
-import formHelper from './components/formHelper.vue'
-
-export default {
-  components: {
-    'form-helper': formHelper
-  },
-
-  data () {
-    return {
-      title: 'I am a dynamic slot title'
+  // Imports
+  import formOne from './components/formOne.vue';
+  import formTwo from './components/formTwo.vue';
+  export default {
+    components: {
+      'form-one': formOne,
+      'form-two': formTwo
+    },
+    data () {
+      return {
+        component: 'form-one'
+      }
+    },
+    methods: {
+      handleSubmit: function(){
+        alert('thanks for submitting');
+      }
     }
   }
-}
 </script>
 
 <style>
-
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
 </style>

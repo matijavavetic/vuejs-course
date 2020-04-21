4f6e5ce7ade816e5fc48f2dc414bfb3ace8d493a
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-04-21 15:10:04 +0200

    ep 43 - posting to firebase

--- Blog/src/components/addBlog.vue
+++ Blog/src/components/addBlog.vue
@@ -62,11 +62,7 @@
 
     methods: {
       post: function() {
-        this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-          title: this.blog.title,
-          body: this.blog.content,
-          userId: 1
-        }).then(function(data) {
+        this.$http.post('https://vue-tutorial-a1d1a.firebaseio.com/posts.json', this.blog).then(function(data) {
             console.log(data);
             this.submitted = true;
         });

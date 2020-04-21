179288a1745aaa8c0cce8720f0ee3c0cb12f3fe0
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-04-21 15:27:23 +0200

    ep 44 - retreiving data from firebase

--- Blog/src/components/showBlog.vue
+++ Blog/src/components/showBlog.vue
@@ -4,7 +4,7 @@
     <input type="text" v-model="search" placeholder="search blogs" />
     <div v-for="blog in filteredBlogs" class="single-blog">
       <router-link v-bind:to="'/blog/' + blog.id"><h2>{{ blog.title | toUppercase }}</h2></router-link>
-      <article>{{ blog.body | snippet }}</article>
+      <article>{{ blog.content | snippet }}</article>
     </div>
   </div>
 </template>
@@ -28,9 +28,15 @@
     },
 
     created() {
-      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (data) {
-        console.log(data);
-        this.blogs = data.body.slice(0,10);
+      this.$http.get('https://vue-tutorial-a1d1a.firebaseio.com/posts.json').then(function (data) {
+        return data.json();
+      }).then(function(data) {
+        var blogsArray = [];
+        for (let key in data) {
+          data[key].id = key;
+          blogsArray.push(data[key]);
+        }
+        this.blogs = blogsArray;
       })
     },
 

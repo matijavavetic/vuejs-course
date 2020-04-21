e95ee3509041230053bbd76b661acfbbd87c48a1
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-04-21 12:38:43 +0200

    ep 36 - custom search filters

--- Blog/src/components/showBlog.vue
+++ Blog/src/components/showBlog.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blog">
     <h1>All Blog Articles</h1>
-    <div v-for="blog in blogs" class="single-blog">
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for="blog in filteredBlogs" class="single-blog">
       <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
       <article>{{ blog.body | snippet }}</article>
     </div>
@@ -12,12 +13,21 @@
   export default {
     data () {
       return {
-        blogs: []
+        blogs: [],
+        search: ''
       }
     },
 
     methods: {
 
+    },
+
+    computed: {
+      filteredBlogs: function() {
+        return this.blogs.filter((blog) => {
+          return blog.title.match(this.search);
+        });
+      }
     },
 
     created() {
